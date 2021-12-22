f6ac952c76c97de36652eba51358f61a7f65a630
rlfah303 <rlarlfah303@gmail.com>
2021-12-22 18:46:31 +0900

    add imageView

--- cameraTest/ViewController.swift
+++ cameraTest/ViewController.swift
@@ -24,9 +24,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didTapButton(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
         
     }
-
-
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+                UIImage else{
+                    return
+                }
+                imageView.image = image
+    }
+}
