fb12ade86a2036f50843d21f782bac5d7ea6c585
rlfah303 <rlarlfah303@gmail.com>
2021-12-22 16:25:11 +0900

    Initial Commit

---
+++ cameraTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cameraTest
+//
+//  Created by GILMO GIM on 12/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
