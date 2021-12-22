e3e4016c1471b222a5d985cfd5925c7ff1654d35
rlfah303 <rlarlfah303@gmail.com>
2021-12-22 18:11:50 +0900

    add button

--- cameraTest/ViewController.swift
+++ cameraTest/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        button.backgroundColor = .systemBlue
+        button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        print("hello world")
+    }
+    
+    @IBAction func didTapButton(){
+        
     }
 
 
